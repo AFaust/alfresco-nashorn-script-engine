a437eb6a9fb99e0ede18e8438bf7699e4f802d6a
AFaust <git@axel-faust.de>
2016-07-09 20:51:52 +0200

    Add print function to console backend

--- repository/src/main/resources/de/axelfaust/alfresco/nashorn/repo/loaders/legacyRootObjects.js
+++ repository/src/main/resources/de/axelfaust/alfresco/nashorn/repo/loaders/legacyRootObjects.js
@@ -1,112 +1,143 @@
 /* globals -require */
 /* globals SimpleLogger: false */
-define('legacyRootObjects', [ 'globalProperties!nashornJavaScriptProcessor.de.axelfaust.alfresco.nashorn.repo.legacyRootObjects.*' ],
-        function legacyRootObjects_loader(legacyRootObjects)
+define('legacyRootObjects', [ 'globalProperties!nashornJavaScriptProcessor.de.axelfaust.alfresco.nashorn.repo.legacyRootObjects.*',
+        'nashorn!Java' ], function legacyRootObjects_loader(legacyRootObjects, Java)
+{
+    'use strict';
+
+    var logger, loader, isObject, propertyPrefix, runtimeRootObjects;
+
+    logger = new SimpleLogger('de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor.loader.legacyRootObjects');
+    propertyPrefix = 'nashornJavaScriptProcessor.de.axelfaust.alfresco.nashorn.repo.legacyRootObjects.';
+    runtimeRootObjects = Java.type('de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptModel').newAssociativeContainer();
+
+    isObject = function legacyRootObjects_loader__isObject(o)
+    {
+        var result = o !== undefined && o !== null && Object.prototype.toString.call(o) === '[object Object]';
+        return result;
+    };
+
+    /**
+     * This loader module provides the capability to load legacy root objects as AMD modules. The set of available legacy root objects is
+     * constructed from AMD modules registered with this loader via
+     * nashornJavaScriptProcessor.de.axelfaust.alfresco.nashorn.repo.legacyRootObjects.* configuration in the alfresco-global.properties
+     * file(s) and the script argument model.
+     * 
+     * @exports legacyRootObject
+     * @author Axel Faust
+     */
+    loader = {
+
+        /**
+         * Registers a custom root object within the currently executed runtime.
+         * 
+         * @instance
+         * @param {string}
+         *            normalizedId - the normalized ID of the module to register
+         * @param {object}
+         *            module - the module to register
+         * @param {boolean}
+         *            secure - whether the module should be made available to secure scripts only
+         */
+        registerRootObject : function legacyRootObjects_loader__registerRootObject(normalizedId, module, secure)
         {
-            'use strict';
+            runtimeRootObjects[normalizedId] = {
+                module : module,
+                secure : secure === true
+            };
+        },
 
-            var logger, loader, isObject, propertyPrefix;
+        /**
+         * Loads a legacy root object from a normalized module ID.
+         * 
+         * @instance
+         * @param {string}
+         *            normalizedId - the normalized ID of the module to load
+         * @param {function}
+         *            require - the context-sensitive require function
+         * @param {function}
+         *            load - the callback to load either a pre-built object as the module result or a script defining a module from a script
+         *            URL
+         */
+        load : function legacyRootObjects_loader__load(normalizedId, require, load)
+        {
+            var result, isSecure, legacyModulePropertyPrefix, legacyModuleId, legacyModuleLoaderName, legacyModuleSecure, resetFn;
 
-            logger = new SimpleLogger('de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor.loader.legacyRootObjects');
-            propertyPrefix = 'nashornJavaScriptProcessor.de.axelfaust.alfresco.nashorn.repo.legacyRootObjects.';
-
-            isObject = function legacyRootObjects_loader__isObject(o)
+            if (normalizedId in runtimeRootObjects)
             {
-                var result = o !== undefined && o !== null && Object.prototype.toString.call(o) === '[object Object]';
-                return result;
-            };
+                result = runtimeRootObjects[normalizedId].module;
+                isSecure = runtimeRootObjects[normalizedId].secure;
+            }
 
-            /**
-             * This loader module provides the capability to load legacy root objects as AMD modules. The set of available legacy root
-             * objects is constructed from AMD modules registered with this loader via
-             * nashornJavaScriptProcessor.de.axelfaust.alfresco.nashorn.repo.legacyRootObjects.* configuration in the
-             * alfresco-global.properties file(s) and the script argument model.
-             * 
-             * @exports legacyRootObject
-             * @author Axel Faust
-             */
-            loader = {
-                /**
-                 * Loads a legacy root object from a normalized module ID.
-                 * 
-                 * @instance
-                 * @param {string}
-                 *            normalizedId - the normalized ID of the module to load
-                 * @param {function}
-                 *            require - the context-sensitive require function
-                 * @param {function}
-                 *            load - the callback to load either a pre-built object as the module result or a script defining a module from
-                 *            a script URL
-                 */
-                load : function legacyRootObjects_loader__load(normalizedId, require, load)
+            if (result === undefined)
+            {
+                legacyModulePropertyPrefix = propertyPrefix + normalizedId;
+                legacyModuleId = legacyRootObjects[legacyModulePropertyPrefix + '.moduleId'];
+
+                if (typeof legacyModuleId === 'string')
                 {
-                    var result, isSecure, legacyModuleId, legacyModuleLoaderName, legacyModuleSecure, resetFn;
+                    legacyModuleLoaderName = legacyRootObjects[legacyModulePropertyPrefix + '.loaderName'];
+                    legacyModuleSecure = legacyRootObjects[legacyModulePropertyPrefix + '.secure'];
 
-                    legacyModuleId = legacyRootObjects[propertyPrefix + normalizedId + '.moduleId'];
-
-                    if (typeof legacyModuleId === 'string')
+                    if (typeof legacyModuleLoaderName === 'string')
                     {
-                        legacyModuleLoaderName = legacyRootObjects[propertyPrefix + normalizedId + '.loaderName'];
-                        legacyModuleSecure = legacyRootObjects[propertyPrefix + normalizedId + '.secure'];
-
-                        if (typeof legacyModuleLoaderName === 'string')
-                        {
-                            legacyModuleId = legacyModuleLoaderName + '!' + legacyModuleId;
-                        }
-                        else
-                        {
-                            legacyModuleId = legacyModuleId;
-                        }
-
-                        isSecure = legacyModuleSecure === true || legacyModuleSecure === 'true';
-                        logger.trace('Loading legacy root object module {} as secure={}', [ legacyModuleId, isSecure ]);
-
-                        // tag our caller as the actual caller to require
-                        resetFn = require.tagCallerScript(true);
-
-                        try
-                        {
-                            require([ legacyModuleId ], function legacyRootObjects_loader__load__legacyRootObject_callback(value)
-                            {
-                                logger.debug('Resolved {} for legacy root object {}', value, legacyModuleId);
-                                result = value;
-                            });
-
-                            resetFn();
-                        }
-                        catch (e)
-                        {
-                            resetFn();
-                            throw e;
-                        }
+                        legacyModuleId = legacyModuleLoaderName + '!' + legacyModuleId;
+                    }
+                    else
+                    {
+                        legacyModuleId = legacyModuleId;
                     }
 
-                    if (result === undefined)
-                    {
-                        // arguments are never considered secure
-                        isSecure = false;
-                        try
-                        {
-                            logger.trace('Loading legacy root object module {} from arguments', normalizedId);
-                            require([ 'args!' + normalizedId ], function legacyRootObjects_loader__load__args_callback(value)
-                            {
-                                logger.debug('Resolved {} from argument {}', value, normalizedId);
-                                result = value;
-                            });
-                        }
-                        catch (e)
-                        {
-                            logger.debug('Failed to load argument {}', normalizedId);
-                            result = undefined;
-                        }
-                    }
+                    isSecure = legacyModuleSecure === true || legacyModuleSecure === 'true';
+                    logger.trace('Loading legacy root object module {} as secure={}', [ legacyModuleId, isSecure ]);
 
-                    load(result, isSecure);
+                    // tag our caller as the actual caller to require
+                    resetFn = require.tagCallerScript(true);
+
+                    try
+                    {
+                        require([ legacyModuleId ], function legacyRootObjects_loader__load__legacyRootObject_callback(value)
+                        {
+                            logger.debug('Resolved {} for legacy root object {}', value, legacyModuleId);
+                            result = value;
+                        });
+
+                        resetFn();
+                    }
+                    catch (e)
+                    {
+                        resetFn();
+                        throw e;
+                    }
                 }
-            };
+            }
 
-            Object.freeze(loader.load);
-            Object.freeze(loader);
+            if (result === undefined)
+            {
+                // arguments are never considered secure
+                isSecure = false;
+                try
+                {
+                    logger.trace('Loading legacy root object module {} from arguments', normalizedId);
+                    require([ 'args!' + normalizedId ], function legacyRootObjects_loader__load__args_callback(value)
+                    {
+                        logger.debug('Resolved {} from argument {}', value, normalizedId);
+                        result = value;
+                    });
+                }
+                catch (e)
+                {
+                    logger.debug('Failed to load argument {}', normalizedId);
+                    result = undefined;
+                }
+            }
 
-            return loader;
-        });
+            load(result, isSecure);
+        }
+    };
+
+    Object.freeze(loader.load);
+    Object.freeze(loader);
+
+    return loader;
+});
