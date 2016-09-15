8203278f90cf7e78c6238879b2aad0d7a9f020bc
AFaust <axel.faust.g@googlemail.com>
2016-09-15 23:44:42 +0200

    Cleanup of nashorn loader test

--- repository/src/test/resources/junit/tests/loaders/nashornTest.js
+++ repository/src/test/resources/junit/tests/loaders/nashornTest.js
@@ -14,12 +14,14 @@
 
 (function nashornTest()
 {
-    // need to bind ctxt early - won't be available when testObj.<fn> is invoked via extracted interface
+    // need to bind ctxt early - won't be available when testObj.<fn> is invoked
+    // via extracted interface
     var testObj, ctxt = context;
 
     testObj = {
 
-        // copied from NashornScriptProcessor - since we limit access to Java we can't obtain it in before fn
+        // copied from NashornScriptProcessor - since we limit access to Java we
+        // can't obtain it in before fn
         defaultInaccessible : [ 'load', 'loadWithNewGlobal', 'exit', 'quit' ],
 
         customInaccessible : [ 'print', 'Packages', 'Java', 'JavaImporter', 'JSAdapter', 'com', 'edu', 'java', 'javax', 'javafx', 'org' ],
@@ -67,7 +69,13 @@
         testInaccessibles : function nashornTest_testInaccessibles(testCase)
         {
             'use strict';
-            var this_ = this, checkFn;
+            var this_ = this, checkFn, Java, Assert;
+
+            require([ 'nashorn!Java' ], function nashornTest_testInaccessibles_requireJavaCallback(JavaModule)
+            {
+                Java = JavaModule;
+            });
+            Assert = Java.type('org.junit.Assert');
 
             checkFn = function nashornTest_testInaccessibles_forEach(variable)
             {
@@ -76,15 +84,15 @@
                 {
                     value = this_[variable];
 
-                    // currently fails because __noSuchProperty__ hook uses closure _this as scope and does not know if caller is strict or not
-                    // throw new Error('Access to ' + variable + ' should have thrown a ReferenceError');
+                    // currently fails because __noSuchProperty__ hook uses
+                    // closure _this as scope and does not know if caller is
+                    // strict or not
+                    // throw new Error('Access to ' + variable + ' should have
+                    // thrown a ReferenceError');
                 }
                 catch (e)
                 {
-                    if (!(e instanceof ReferenceError))
-                    {
-                        throw new Error('Access to ' + variable + ' should have thrown a ReferenceError');
-                    }
+                    Assert.assertTrue('Access to ' + variable + ' should have thrown a ReferenceError', e instanceof ReferenceError);
                 }
             };
 
@@ -96,184 +104,103 @@
         testJavaAccess : function nashornTest_testJavaAccess(testCase)
         {
             'use strict';
-            // we don't test all but most relevant fns to check for "real" NativeJava
-            var expected, javaTestFns = [ 'type', 'typeName', 'from', 'to', 'extend' ];
-            try
+            // we don't test all but most relevant fns to check for "real"
+            // NativeJava
+            require([ 'nashorn!Packages', 'nashorn!Java' ], function nashornTest_testJavaAccess_loaded(Packages, Java)
             {
-                require([ 'nashorn!Java' ], function nashornTest_testJavaAccess_loaded(Java)
+                var Assert, testClass, javaTestFns;
+
+                Assert = Packages.org.junit.Assert;
+                javaTestFns = [ 'type', 'typeName', 'from', 'to', 'extend' ]
+
+                javaTestFns.forEach(function nashornTest_testJavaAccess_loaded_fnCheck(fnName)
                 {
-                    var testClass;
-
-                    javaTestFns.forEach(function nashornTest_testJavaAccess_loaded_fnCheck(fnName)
-                    {
-                        if (typeof Java[fnName] !== 'function')
-                        {
-                            expected = new Error('NativeJava should provide function ' + fnName);
-                            throw expected;
-                        }
-                    });
-
-                    testClass = Java.type('de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor');
-
-                    if (testClass === undefined || testClass === null || testClass.class === undefined || testClass.class === null
-                            || testClass.class.name !== 'de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor')
-                    {
-                        expected = new Error('NativeJava.type() should have been able to provide static class for NashornScriptProcessor');
-                        throw expected;
-                    }
-
-                }, function nashornTest_testJavaAccess_failed(dependencies, modules, implicitModules)
-                {
-                    expected = new Error('NativeJava should have been provided via module nashorn!Java');
-                    throw expected;
+                    Assert.assertTrue(fnName + ' is not a function', typeof Java[fnName] === 'function');
                 });
-            }
-            catch (e)
-            {
-                if (e === expected)
-                {
-                    throw e;
-                }
-                throw new Error('NativeJava should have been provided via module nashorn!Java - failed instead with ' + e);
-            }
+
+                testClass = Java.type('de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor');
+
+                Assert.assertFalse('testClass was resolved as undefined', testClass === undefined);
+                Assert.assertNotNull('testClass was resolved as null', testClass);
+                Assert.assertFalse('testClass.class is undefined', testClass.class === undefined);
+                Assert.assertNotNull('testClass.class is null', testClass.class);
+                Assert.assertEquals('testClass is not the expected class',
+                        'de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor', testClass.class.name);
+            });
         },
 
         testPackagesAccess : function nashornTest_testPackagesAccess(testCase)
         {
             'use strict';
-            var expected;
-            try
+            require([ 'nashorn!Java', 'nashorn!Packages' ], function nashornTest_testPackagesAccess_loaded(Java, Packages)
             {
-                require([ 'nashorn!Packages' ], function nashornTest_testPackagesAccess_loaded(Packages)
-                {
-                    var testClass;
+                var Assert, testClass;
 
-                    testClass = Packages.de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor;
+                Assert = Java.type('org.junit.Assert');
+                testClass = Packages.de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor;
 
-                    if (testClass === undefined || testClass === null || testClass.class === undefined || testClass.class === null
-                            || testClass.class.name !== 'de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor')
-                    {
-                        expected = new Error('Packages should have been able to provide static class for NashornScriptProcessor');
-                        throw expected;
-                    }
-
-                }, function nashornTest_testPackagesAccess_failed(dependencies, modules, implicitModules)
-                {
-                    expected = new Error('Packages should have been provided via module nashorn!Packages');
-                    throw expected;
-                });
-            }
-            catch (e)
-            {
-                if (e === expected)
-                {
-                    throw e;
-                }
-                throw new Error('Packages should have been provided via module nashorn!Packages - failed instead with ' + e);
-            }
+                Assert.assertFalse('testClass was resolved as undefined', testClass === undefined);
+                Assert.assertNotNull('testClass was resolved as null', testClass);
+                Assert.assertFalse('testClass.class is undefined', testClass.class === undefined);
+                Assert.assertNotNull('testClass.class is null', testClass.class);
+                Assert.assertEquals('testClass is not the expected class',
+                        'de.axelfaust.alfresco.nashorn.repo.processor.NashornScriptProcessor', testClass.class.name);
+            });
         },
 
         testJSAdapterAccess : function nashornTest_testJSAdapterAccess(testCase)
         {
             'use strict';
-            var expected;
-            try
+            require([ 'nashorn!JSAdapter', 'nashorn!Packages' ], function nashornTest_testJSAdapterAccess_loaded(JSAdapter, Packages)
             {
-                require([ 'nashorn!JSAdapter', 'nashorn!Packages' ], function nashornTest_testJSAdapterAccess_loaded(JSAdapter, Packages)
-                {
-                    var Assert, proxy, adaptee;
-                    Assert = Packages.org.junit.Assert;
+                var Assert, proxy, adaptee;
+                Assert = Packages.org.junit.Assert;
 
-                    adaptee = {
-                        __get__ : function __get__(name)
-                        {
-                            return 'simulated prop "' + name + '"';
-                        },
-
-                        __call__ : function __call__(name, arg1, arg2)
-                        {
-                            return 'simulated fn "' + name + '"';
-                        }
-                    };
-
-                    try
+                adaptee = {
+                    __get__ : function __get__(name)
                     {
-                        proxy = new JSAdapter(adaptee);
+                        return 'simulated prop "' + name + '"';
+                    },
 
-                        Assert.assertEquals('simulated prop "x"', proxy.x);
-                        Assert.assertEquals('simulated fn "getX"', proxy.getX());
-                    }
-                    catch (innerE)
+                    __call__ : function __call__(name, arg1, arg2)
                     {
-                        expected = innerE;
-                        throw innerE;
+                        return 'simulated fn "' + name + '"';
                     }
-                }, function nashornTest_testJSAdapterAccess_failed(dependencies, modules, implicitModules)
-                {
-                    expected = new Error('JSAdapter should have been provided via module nashorn!JSAdapter');
-                    throw expected;
-                });
-            }
-            catch (e)
-            {
-                if (e === expected)
-                {
-                    throw e;
-                }
-                throw new Error('JSAdapter should have been provided via module nashorn!JSAdapter - failed instead with ' + e);
-            }
+                };
+
+                proxy = new JSAdapter(adaptee);
+
+                Assert.assertEquals('property x did not yield expected value', 'simulated prop "x"', proxy.x);
+                Assert.assertEquals('getter for property x did not yield expected value', 'simulated fn "getX"', proxy.getX());
+            });
         },
 
         testJavaImporterAccess : function nashornTest_testJavaImporterAccess(testCase)
         {
             // not "use strict;" since that would disallow use of with
-            var expected;
-            try
+            require([ 'nashorn!JavaImporter', 'nashorn!Packages' ], function nashornTest_testJavaImporterAccess_loaded(JavaImporter,
+                    Packages)
             {
-                require([ 'nashorn!JavaImporter', 'nashorn!Packages' ], function nashornTest_testJavaImporterAccess_loaded(JavaImporter,
-                        Packages)
+                var Assert, imports, map, list;
+
+                Assert = Packages.org.junit.Assert
+                imports = new JavaImporter(Packages.java.util);
+
+                with (imports)
                 {
-                    var Assert, imports, map, list;
+                    Assert.assertTrue(typeof HashMap === 'function');
+                    Assert.assertTrue(typeof ArrayList === 'function');
 
-                    Assert = Packages.org.junit.Assert;
-                    try
-                    {
-                        imports = new JavaImporter(Packages.java.util);
+                    map = new HashMap();
+                    Assert.assertEquals('java.util.HashMap', map.class.name);
+                    Assert['assertEquals(long,long)'](0, map.size());
 
-                        with (imports)
-                        {
-                            Assert.assertTrue(typeof HashMap === 'function');
-                            Assert.assertTrue(typeof ArrayList === 'function');
-
-                            map = new HashMap();
-                            Assert.assertEquals('java.util.HashMap', map.class.name);
-                            Assert['assertEquals(long,long)'](0, map.size());
-
-                            list = new ArrayList();
-                            Assert.assertEquals('java.util.ArrayList', list.class.name);
-                            Assert['assertEquals(long,long)'](0, list.length);
-                            Assert['assertEquals(long,long)'](0, list.size());
-                        }
-                    }
-                    catch (innerE)
-                    {
-                        expected = innerE;
-                        throw innerE;
-                    }
-                }, function nashornTest_testJavaImporterAccess_failed(dependencies, modules, implicitModules)
-                {
-                    expected = new Error('JavaImporter should have been provided via module nashorn!JavaImporter');
-                    throw expected;
-                });
-            }
-            catch (e)
-            {
-                if (e === expected)
-                {
-                    throw e;
+                    list = new ArrayList();
+                    Assert.assertEquals('java.util.ArrayList', list.class.name);
+                    Assert['assertEquals(long,long)'](0, list.length);
+                    Assert['assertEquals(long,long)'](0, list.size());
                 }
-                throw new Error('JavaImporter should have been provided via module nashorn!JavaImporter - failed instead with ' + e);
-            }
+            });
         }
     };
     return testObj;
